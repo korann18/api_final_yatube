d3163cf3a02a40e1396900a32b17530dc8c7b9ca
korann18 <60140258+korann18@users.noreply.github.com>
2021-09-07 22:58:03 +0200

    commit 1.0

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# api_final
-api final
+# API для Yatube
+
+###Что это за проект?
+Мы создали API для приложения Yatube!
+
+API (от англ. Application Programming Interface, «программный интерфейс приложения») — это интерфейс для обмена данными. Слово «программный» означает, что API служат в первую очередь для взаимодействия программ: с системой взаимодействует не разработчик, а код, написанный им.
+
+С помощью этого API можно получить токен, разместить информацию в своем блоге, подписаться на пользователей и многое другое.
+
+###Как запустить проект:
+
+Клонировать репозиторий и перейти в него в командной строке:
+```
+git clone https://github.com/korann18/api_final_yatube.git
+```
+но git clone не сработает, потому что GitHub поменял политику :upside_down_face:
+```
+cd yatube_api 
+```
+Cоздать и активировать виртуальное окружение:
+```
+python3 -m venv venv
+```
+```
+source venv/bin/activate
+```
+Установить зависимости из файла requirements.txt:
+```
+python3 -m pip install --upgrade pip
+```
+```
+pip install -r requirements.txt
+```
+Выполнить миграции:
+```
+python3 manage.py migrate
+```
+Запустить проект:
+```
+python3 manage.py runserver
+```
+
+###Примеры запросов к API
+
+Получение публикаций: 
+####GET
+```
+http://127.0.0.1:8000/api/v1/posts/
+```
+```
+{
+  "count": 123,
+  "next": "http://api.example.org/accounts/?offset=400&limit=100",
+  "previous": "http://api.example.org/accounts/?offset=200&limit=100",
+  "results": [
+    {
+      "id": null,
+      "author": null,
+      "text": null,
+      "pub_date": "2019-08-24T14:15:22Z",
+      "image": "string",
+      "group": null
+    }
+  ]
+}
+```
+
+Создание публикации:
+####POST
+```
+{
+  "text": "string",
+}
+```
+```
+http://127.0.0.1:8000/api/v1/posts/
+```
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,50 @@
-# TODO:  Напишите свой вариант
+from rest_framework import filters, viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+
+from posts.models import Follow, Group, Post, User
+
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user,
+                        following=get_object_or_404(User, username=self.request.data.get('following')))
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user)
